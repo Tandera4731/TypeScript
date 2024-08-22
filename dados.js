49354d66cda1b03a22b32e023cbd06ed0374fe77
Tandera4731 <victoriopereira30@gmail.com>
2024-08-22 19:34:36 -0300

    First Commit

---
+++ dados.js
@@ -0,0 +1,6 @@
+var faculdade = "Fiap";
+var sala = 203;
+var inervalo = false;
+console.log("Faculdade: ".concat(faculdade));
+console.log("Sala: ".concat(sala));
+console.log("Inervalo: ".concat(inervalo));
---
+++ dados.ts
@@ -0,0 +1,6 @@
+let faculdade: string = "Fiap";
+let sala: number = 203;
+let inervalo: boolean = false;
+console.log(`Faculdade: ${faculdade}`);
+console.log(`Sala: ${sala}`);
+console.log(`Inervalo: ${inervalo}`);
